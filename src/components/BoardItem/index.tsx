e7e2878b28a9862e7e44819ffb7805b2dcc0f969
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-05-28 00:15:24 +0900

    component boardItem

--- src/components/BoardItem/index.tsx
+++ src/components/BoardItem/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './style.css';
 import {BoardListItem} from 'types/interface';
 import {useNavigate} from "react-router-dom";
+import defaultProfileImage from 'assets/image/default-profile-image.png';
 
 interface Props {
     boardListItem: BoardListItem
 }
 
 // component: board List Item 컴포넌트
-export default function BoardListItem({boardListItem}: Props) {
+export default function BoardItem({boardListItem}: Props) {
 
     // propertises //
     const {boardNumber, title, content, boardTitleImage} = boardListItem;
@@ -17,12 +18,12 @@ export default function BoardListItem({boardListItem}: Props) {
 
     // function : 내비게이트 함수 //
 
-    const navigator = useNavigate();
+    //const navigator = useNavigate();
 
     // event handler : 게시물 아이템 클릭 이벤트 처리함수 //
 
     const onClickHandler = () => {
-        navigator(boardNumber);
+       // navigator(boardNumber);
     }
 
     // render : Board List Item 컴포넌트 렌더링
@@ -32,7 +33,7 @@ export default function BoardListItem({boardListItem}: Props) {
                 <div className='board-list-item-top'>
                     <div className='board-list-item-profile-box'>
                         <div className='board-list-item-profile-image'
-                             style={{backgroundImage: `url(${writerProfileImage})`}}></div>
+                             style={{backgroundImage: `url(${writerProfileImage ? writerProfileImage : defaultProfileImage})`}}></div>
 
                     </div>
                     <div className='board-list-item-write-box'>
---
+++ src/mocks/latest-board-list.mock.ts
@@ -0,0 +1,20 @@
+import {BoardListItem} from 'types/interface';
+
+const latestBoardListMock: BoardListItem[] = [
+    {
+        "boardNumber": 1,
+        "title":  "졸리다 자고싶다",
+        "content": "너무너무 자고싶다",
+        "boardTitleImage": 'https://cdn.indiepost.co.kr/uploads/images/2017/11/14/Lis0NS-660x321.jpeg',
+        "favoriteCount": 0,
+        "commentCount": 0,
+        "viewCount": 0,
+        "writeDatetime": "2024.05.27. 23:59",
+        "writerNickname": "코리안 몬스터",
+        "writerProfileImage": 'https://www.instantaiprompt.com/wp-content/uploads/2023/12/Luna-Lovegood-pixar-ai.jpg',
+
+    }
+
+];
+
+export default latestBoardListMock;
