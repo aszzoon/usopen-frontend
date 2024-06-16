584a58c3cbb92b2f3f74419d2e5038c463198e27
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-06-16 19:36:00 +0900

    Top3 board list mock

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,14 @@ import './App.css';
 import BoardItem from "./components/BoardItem";
 import latestBoardListMock from "./mocks/latest-board-list.mock";
 import Top3Item from "./components/Top3Item";
+import {top3BoardListMock} from "./mocks";
 
 function App() {
     return (
         <>
             {/*{latestBoardListMock.map(boardListItem => <BoardItem boardListItem={boardListItem}/>)}*/}
         <div style={{display: 'flex', justifyContent: 'center', gap : '24px'}}>
-            <Top3Item />
-            <Top3Item />
-            <Top3Item />
+            {top3BoardListMock.map(top3ListItem => <Top3Item top3ListItem={top3ListItem} />)}
         </div>
 
 
--- src/components/Top3Item/index.tsx
+++ src/components/Top3Item/index.tsx
@@ -1,28 +1,51 @@
 import React from "react";
 import './style.css';
+import defaultProfileImage from 'assets/image/default-profile-image.png'
+import { BoardListItem } from 'types/interface';
+import {useNavigate} from "react-router-dom";
+
+interface Props {
+    top3ListItem: BoardListItem
+}
 
 //      component : Top 3 List Item 컴포넌트        //
-export default function Top3Item() {
+export default function Top3Item({ top3ListItem } : Props) {
+
+    // properties //
+
+    const { boardNumber, title, content, boardTitleImage } = top3ListItem;
+    const { favoriteCount, commentCount,viewCount } = top3ListItem;
+    const { writeDatetime, writerNickname, writerProfileImage } = top3ListItem
+
+    // function : 네비게이트 함수 //
+
+    // const navigator = useNavigate();
+
+    // event handler : 탑 3 아이템 클릭 이벤트 처리 함수 //
+    const onClickHandler = () => {
+        // navigator(boardNumber);
+    }
 
     //      render : Top 3 List Item 컴포넌트 렌더링        //
     return (
-        <div className='top-3-list-item'>
+        <div className='top-3-list-item' style={{backgroundImage : `url(${boardTitleImage}`}} onClick={onClickHandler}>
             <div className='top-3-list-item-main-box'>
                 <div className='top-3-list-item-top'>
                     <div className='top-3-list-item-profile-box'>
-                        <div className='top-3-list-item-profile-image' style={{backgroundImage: 'url()'}}></div>
+                        <div className='top-3-list-item-profile-image' style={{backgroundImage: `url(${writerProfileImage ? writerProfileImage : defaultProfileImage})`}}></div>
                     </div>
                     <div className='top-3-list-item-write-box'>
-                        <div className='top-3-list-item-nickname'>{'hello world'}</div>
-                        <div className='top-3-list-item-write-date'>{'2024.05.30.'}</div>
+                        <div className='top-3-list-item-nickname'><div                            className='top-3-list-item-nickname'>{writerNickname}</div>
+                        </div>
+                        <div className='top-3-list-item-write-date'>{writeDatetime}</div>
                     </div>
                 </div>
                 <div className='top-3-list-item-middle'>
-                    <div className='top-3-list-item-title'>{'오늘은 목요일 내일은 금요일 모레는 토요일'}</div>
-                    <div className='top-3-list-item-content'>{'다음주는 월요일 화요일 수요일 목요일 금요일 토요일'}</div>
+                    <div className='top-3-list-item-title'>{title}</div>
+                    <div className='top-3-list-item-content'>{content}</div>
                 </div>
                 <div className='top-3-list-item-bottom'>
-                    <div className='top-3-list-item-counts'>{'댓글 0 * 좋아요 0 * 조회수 0'}</div>
+                    <div className='top-3-list-item-counts'>{`댓글${commentCount} * 좋아요 ${favoriteCount} * 조회수 ${viewCount}`}</div>
                 </div>
             </div>
         </div>
