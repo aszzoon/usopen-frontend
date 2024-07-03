65124f54dcdde36c4f5c24fd5bb8c12b518ba4ab
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-07-03 12:03:38 +0900

    .idea

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,18 +1,27 @@
 import React, {forwardRef} from 'react'
 import './style.css'
 
+// interface: Input Box 컴포넌트 Propperties //
 interface Props {
-
+    label: string;
+    type: 'text | password';
+    placeholder : string;
+    value: string;
+    error: boolean;
 }
 
-
+// componenet : Input Box 컴포넌트 //
 const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
 
+    // state: properties //
+    const { label, type, placeholder, value, error } = props;
+
+    // render: Input box 컴포넌트 //
     return (
         <div className='inputbox'>
-            <div className='inputbox-label'>{'비밀번호*'}</div>
-            <div className='inputbox-container'>
-                <input className='input'/>
+            <div className='inputbox-label'>{label}</div>
+            <div className={error ? 'inputbox-container-error' : 'inputbox-container'}>
+                <input type={type} className='input' placeholder={placeholder} value={value}/>
                 <div className='icon-button'>
                     <div className='icon eye-light-off-icon'></div>
                 </div>
