49a6e661405f141dd71e7d00c862a8609415facd
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-07-06 09:03:49 +0900

    InputBox components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import BoardItem from "./components/BoardItem";
 import latestBoardListMock from "./mocks/latest-board-list.mock";
@@ -6,17 +6,17 @@ import Top3Item from "./components/Top3Item";
 import {commentListMock, favoriteListMock, top3BoardListMock} from "./mocks";
 import CommentItem from "./components/CommentItem";
 import FavoriteItem from 'components/FavoriteItem';
+import InputBox from "./components/InputBox";
+//import {useNavigate} from "react-router-dom";
 
 
 function App() {
+
+    const [value, setValue] = useState<string>('');
     return (
         <>
-            <div style={{display: 'flex', columnGap: '30px', rowGap: '20px'}}>
-
-                {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem}/>)}
-
-            </div>
-
+            <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={true} setValue={setValue} message='aaaaa'
+            />
         </>
     );
 }
--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,19 +1,21 @@
-import React, {ChangeEvent, Dispatch, forwardRef, SetStateAction} from 'react'
+import React, {ChangeEvent, Dispatch, forwardRef, KeyboardEvent, SetStateAction} from 'react'
 import './style.css'
 
 // interface: Input Box 컴포넌트 Propperties //
 interface Props {
     label: string;
-    type: 'text | password';
+    type: 'text' | 'password';
     placeholder: string;
     value: string;
-    setValue: Dispatch<SetStateAction<String>>;
+    setValue: Dispatch<SetStateAction<string>>;
     error: boolean;
 
     icon?: string;
     onButtonClick?: () => void;
 
     message?: string;
+
+    onKeyDown?: (event: KeyboardEvent<HTMLInputElement>) => void;
 }
 
 // componenet : Input Box 컴포넌트 //
@@ -21,31 +23,35 @@ const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
 
     // state: properties //
     const {label, type, placeholder, value, error, icon, message} = props;
-    const {setValue, onButtonClick} = props;
+    const {setValue, onButtonClick, onKeyDown} = props;
     // event handler : input 값 변경 이벤트 처리 함수 //
     const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
         const {value} = event.target;
         setValue(value);
     }
-
+// event handler : input 키 이벤트 처리 함수 //
+    const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (!onKeyDown) return;
+        onKeyDown(event);
+    }
 
     // render: Input box 컴포넌트 //
     return (
-
         <div className='inputbox'>
             <div className='inputbox-label'>{label}</div>
             <div className={error ? 'inputbox-container-error' : 'inputbox-container'}>
-                <input type={type} className='input' placeholder={placeholder} value={value}
-                       onChange={onChangeHandler}/>
+                <input ref={ref} type={type} className='input' placeholder={placeholder} value={value}
+                       onChange={onChangeHandler} onKeyDown={onKeyDownHandler} />
                 {onButtonClick !== undefined && (
                     <div className='icon-button'>
-                        {icon !== undefined && (<div className={`icon ${icon}`}/>)}</div>)}
+                        {icon !== undefined && (<div className={`icon ${icon}`}></div>)}
+                    </div>
+                )}
             </div>
-            )}
+            {message !== undefined && <div className='inputbox-message'> (message)</div>}
+            <div className='inputbox-message'>{'비밀번호는 8자 이상 입력해주세요.'}</div>
         </div>
-    <div className='inputbox-message'>{'비밀번호는 8자 이상 입력해주세요.'}</div>
-
-)
+    )
 });
 
 export default InputBox;
